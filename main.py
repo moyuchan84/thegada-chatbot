1a7889996d18b43ea3b325556c01e5e162061f94
yuchan <mojjijji2@gmail.com>
2025-07-28 20:59:03 +0900

    save

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ app.include_router(validate_router.router, prefix="/api/validate", tags=["valida
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
